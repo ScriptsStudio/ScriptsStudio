62f35134252d58586ab65cc531d937b60fc37b17
Axlfc <axelfernandezcurros@gmail.com>
2021-09-30 15:41:49 +0200

    CHANGES to manage_apt_repo.sh

--- scripts/manage_apt_repo.sh
+++ scripts/manage_apt_repo.sh
@@ -1,4 +1,4 @@
-# Expected to be run by user with sudo privileges and source this script from ~/.bashrc
+# Expected to be run by user with sudo privileges and source this script from script_source_list.sh
 addrepo() {
   sudo add-apt-repository -y "$1"
   sudo apt-get update -y
