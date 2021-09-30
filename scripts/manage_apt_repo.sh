e0f686e2088fc3cec3d2527b070e0541a26c6db1
Axlfc <axelfernandezcurros@gmail.com>
2021-09-30 15:39:02 +0200

    CHANGES: to addrepo and delrepo

--- scripts/manage_apt_repo.sh
+++ scripts/manage_apt_repo.sh
@@ -1,8 +1,10 @@
-# Expected to be run with superuser privileges
+# Expected to be run by user with sudo privileges and source this script from ~/.bashrc
 addrepo() {
-  add-apt-repository "$1"
+  sudo add-apt-repository -y "$1"
+  sudo apt-get update -y
 }
 
 delrepo() {
-  add-apt-repository --remove "$1"
+  sudo add-apt-repository --remove -y "$1"
+  sudo apt-get update -y
 }
