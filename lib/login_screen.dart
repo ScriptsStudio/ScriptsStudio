df8dba6f73c902b4a96ca24a1f7b8b33639fbda4
David <46814441+XRuppy@users.noreply.github.com>
2021-10-11 18:55:06 +0200

    Setting theme for typography, LoginScreen redesign (Unfinished), etc.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,11 +1,35 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 
-late List ipsList = [];
-late List hostnameList = [];
-
+late List ipsList = [
+  '192.168.1.2',
+  '192.168.1.3',
+  '192.168.1.4',
+  '192.168.1.5',
+  '192.168.1.6',
+  '192.168.1.7',
+  '192.168.1.8',
+  '192.168.1.9',
+  '192.168.1.10'
+];
+late List hostnameList = [
+  'PC de David',
+  'MacBook',
+  'Windows-SSD',
+  'Windows 10',
+  'Ubuntu-20',
+  'Arch Linux',
+  'Mac Mini',
+  'Windows 11',
+  'MSI-Axel',
+  '1',
+  '1',
+  '2',
+  '1'
+];
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -21,30 +45,32 @@ class _LoginScreenState extends State<LoginScreen> {
     scanNetwork();
     super.initState();
   }
+
   Future<void> scanNetwork() async {
     await (NetworkInfo().getWifiIP()).then(
-          (ip) async {
+      (ip) async {
         final String subnet = ip!.substring(0, ip.lastIndexOf('.'));
         const port = 22;
         for (var i = 0; i < 256; i++) {
           String ip = '$subnet.$i';
           await Socket.connect(ip, port, timeout: Duration(milliseconds: 50))
               .then((socket) async {
-            await InternetAddress(socket.address.address).reverse().then((value) {
+            await InternetAddress(socket.address.address)
+                .reverse()
+                .then((value) {
               setState(() {
                 ipsList.add(socket.address.address);
                 hostnameList.add(value.host);
                 print(socket.address.address);
                 print(value.host);
               });
-
             }).catchError((error) {
               print(socket.address.address);
               print('catchError InternetAddress: $error');
             });
             socket.destroy();
-          }).catchError((error)  {
-            print('catchError Socket.connect'+error.toString());
+          }).catchError((error) {
+            print('catchError Socket.connect' + error.toString());
           });
         }
       },
@@ -58,106 +84,110 @@ class _LoginScreenState extends State<LoginScreen> {
           child: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Login',
-                style: TextStyle(color: Colors.white, fontSize: 50),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Manual connection',
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(60)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12.withOpacity(0.2),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 40.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.redAccent,
+                    child: Text('Logo'),
+                  ),
                 ),
-                height: MediaQuery.of(context).size.height * 0.9,
-                width: MediaQuery.of(context).size.width * 0.9,
-              ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 5.0, vertical: 40.0),
+                  child: Text('ScriptsStudio',
+                      style: Theme.of(context).textTheme.headline4),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Automatic connection',
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-            Center(
-              child: Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(60)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12.withOpacity(0.2),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      height: MediaQuery.of(context).size.height * 0.9,
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: ListView.builder(
-                          itemCount: ipsList.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                                elevation: 2,
-                                shadowColor: Colors.black,
-                                color: Colors.white70,
-                                child: Column(
-                                  children: [
-                                    ListTile(
-                                      title: Text(hostnameList[index]),
-                                      subtitle: Text(ipsList[index]),
-                                    ),
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        ButtonBar(
+            Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40),
+                          topLeft: Radius.circular(40)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text('Automatic connection',
+                              style: Theme.of(context).textTheme.subtitle1),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: GridView.count(
+                                  crossAxisCount: 3,
+                                  scrollDirection: Axis.vertical,
+                                  shrinkWrap: true,
+                                  crossAxisSpacing:2,
+                                  mainAxisSpacing: 2,
+                                  children:
+                                      List.generate(ipsList.length, (index) {
+                                    return Card(
+                                        shape: RoundedRectangleBorder(
+                                          side: BorderSide(color: Colors.black),
+                                          borderRadius:
+                                              BorderRadius.circular(30.0),
+                                        ),
+                                        elevation: 6,
+                                        shadowColor: Colors.black,
+                                        color: Colors.white,
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          mainAxisAlignment: MainAxisAlignment.center,
                                           children: [
-                                            IconButton(
-                                              onPressed: null,
-                                              icon: Icon(Icons.add_circle_outline),
-                                              color: Colors.redAccent,tooltip: 'Connect to host',
+                                            Expanded(
+                                              child: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    top: 8.0),
+                                                child: CircleAvatar(
+                                                  child: Icon(
+                                                    Icons.computer,
+                                                    color: Colors.white,
+                                                  ),
+                                                  backgroundColor:
+                                                      Colors.redAccent,
+                                                ),
+                                              ),
+                                            ),
+                                            ListTile(
+                                              title: Text(hostnameList[index],
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodyText1),
+                                              subtitle: Text(ipsList[index],
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodyText2),
                                             ),
                                           ],
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                ));
-                          },
+                                        ));
+                                  }))),
                         ),
-                      ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,38 @@ class MyApp extends StatelessWidget {
         const Locale('en'), // Inglés
         const Locale('es'), // Español
         const Locale('ca'), // Catalán
-
       ],
       debugShowCheckedModeBanner: false,
       title: 'ScriptStudio',
-      theme: ThemeData(fontFamily: "Product Sans",visualDensity: VisualDensity.adaptivePlatformDensity,
-        primaryColor: Colors.red, accentColor: Colors.redAccent,scaffoldBackgroundColor:Colors.red),
+      theme: ThemeData(
+        fontFamily: "Product Sans",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.red,
+        accentColor: Colors.redAccent,
+        scaffoldBackgroundColor: Colors.red,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+              fontSize: 96.0, fontWeight: FontWeight.bold, letterSpacing: -1.5,color: Colors.white),
+          headline2: TextStyle(
+              fontSize: 60.0, fontWeight: FontWeight.bold, letterSpacing: -0.5,color: Colors.white),
+          headline3: TextStyle(
+              fontSize: 48.0, fontWeight: FontWeight.bold, letterSpacing: 0,color: Colors.white),
+          headline4: TextStyle(
+              fontSize: 34.0, fontWeight: FontWeight.bold, letterSpacing: 0.25,color: Colors.white),
+          headline5: TextStyle(
+              fontSize: 24.0, fontWeight: FontWeight.bold, letterSpacing: 0,color: Colors.white),
+          headline6: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold, letterSpacing: 0.15,color: Colors.white),
+          bodyText1: TextStyle(fontSize: 16.0, letterSpacing: 0.5),
+          bodyText2: TextStyle(fontSize: 14.0, letterSpacing: 0.25),
+          button: TextStyle(fontSize: 14.0,fontWeight: FontWeight.bold, letterSpacing: 1.25,color: Colors.white),
+          caption: TextStyle(fontSize: 12.0, letterSpacing: 0.4),
+          overline: TextStyle(fontSize: 10.0, letterSpacing: 1.5),
+          subtitle1: TextStyle(fontSize: 18.0, letterSpacing: 0.15, fontStyle: FontStyle.italic
+          ),
+          subtitle2: TextStyle(fontSize: 16.0, letterSpacing: 0.1, fontStyle: FontStyle.italic),
+        ),
+      ),
       home: LoginScreen(),
     );
   }
