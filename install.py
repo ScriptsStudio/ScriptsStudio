905d673f014a55474c1c9cfc049f47e42b272978
Axlfc <axelfernandezcurros@gmail.com>
2021-10-13 11:44:09 +0200

    CREATE: Python script to manage multiplatform basic installations

---
+++ install.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import subprocess
+import platform
+import sys
+
+
+def initialize_package_manager_command():
+    os_name = platform.system()
+    if os_name == "Linux":
+        # This value might vary between flatpak, snapd, apt-get or Linux-Auto-Customizer
+        package_manager_command="sudo apt-get install -y "
+    elif os_name == "Windows":
+        package_manager_command="winget install "
+    elif os_name == "Darwin":
+        package_manager_command="brew install "
+    return package_manager_command
+
+
+if __name__ == "__main__":
+    argument_list = sys.argv[1:]
+    for arg in argument_list:
+        try:
+            subprocess.run(initialize_package_manager_command() + arg, shell=True)
+        except:
+            print("ERROR: Could not launch installation")
+            exit(1)
