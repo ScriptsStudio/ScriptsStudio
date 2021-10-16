af67a61ec1294dff53ffe6ed95ca6da135fb885c
Axlfc <axel.fernandez@mercatsdetarragona.cat>
2021-10-16 16:57:50 +0200

    UPDATE: arguments unification

--- install.py
+++ install.py
@@ -6,33 +6,35 @@ import sys
 
 def initialize_package_manager_command():
     os_name = platform.system()
-    package_manager_command=""
+    package_manager_command = ""
+
+    # This expects a path to an .apk file and root (sudo) privileges assuming rooted android device
     if subprocess.check_output(['uname', '-o']).strip() == b'Android':
-        # This expects a path to an .apk file and root (sudo) privileges
-        package_manager_command="pm install "
-        return package_manager_command
-        
-    if os_name == "Linux":
+            package_manager_command = "pm install "    
+    elif os_name == "Linux":
+        pacman_names = ["apt-get install -y ", "customizer-install ", "flatpak install ", "snap install "]
         # This value might vary between flatpak, snapd, apt-get or Linux-Auto-Customizer
-        # For the moment expects a valid app name and root (sudo) privileges
-        package_manager_command="apt-get install -y "
+        # For the moment expects a valid app name and may need root (sudo) privileges
+        package_manager_command = pacman_names[1]
     elif os_name == "Windows":
-        package_manager_command="winget install "
+        package_manager_command = "winget install "
     elif os_name == "Darwin":
-        package_manager_command="brew install "
+        package_manager_command = "brew install "
+    else:
+        # We assume arch Linux user
+        other_pacman_names = ["apt install -y " "pacman install ", "choco install ", "scoop install "]
+        package_manager_command = other_pacman_names[1]
     return package_manager_command
 
 
 if __name__ == "__main__":
     argument_list = sys.argv[1:]
-    for arg in argument_list:
-        try:
-            # Trim '-' and '--' before package name
-            if arg[:1] == "-":
-                arg = arg[1:]
-            if arg[:1] == "-":
-                arg = arg[1:]
-            subprocess.run(initialize_package_manager_command() + arg, shell=True)
-        except:
-            print("ERROR: Could not launch installation")
-            exit(1)
+    try:
+        quer = initialize_package_manager_command()
+        for arg in argument_list:
+            arg += " "
+            quer += arg
+        subprocess.run(quer, shell=True)
+    except:
+        print("ERROR: Could not launch installation")
+        exit(1)
