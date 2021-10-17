d57f9437948cf8c1051c24d4242e5d754d3b13e2
Axel <axelfernandezcurros@gmail.com>
2021-10-17 17:55:42 +0200

    UPDATE: install.py
    Adapted to windows to install several programs one-liners

--- install.py
+++ install.py
@@ -7,30 +7,45 @@ import sys
 def main():
     argument_list = sys.argv[1:]
     opsy_name = platform.system()
+    winget_allowance_command = False
     package_manager_command = ""
-    final_query = ""
-
+    quer = ""
+    query_list = []
+    final_query = []
     # This expects a path to an .apk file and root (sudo) privileges assuming rooted android device
-    if subprocess.check_output(['uname', '-o']).strip() == 'Android':
-        package_manager_command = "pm install "
-        return package_manager_command
+    if opsy_name == "Linux":
+        pacman_names = ["apt-get install -y ", "customizer-install ", "flatpak install ", "snap install "]
+        # This value might vary between flatpak, snapd, apt-get or Linux-Auto-Customizer
+        # For the moment expects a valid app name and may need root (sudo) privileges
+        package_manager_command = pacman_names[1]
+    elif opsy_name == "Windows":
+        package_manager_command = "winget install --accept-package-agreements --accept-source-agreements -h -q "
+        winget_allowance_command = True
+    elif opsy_name == "Darwin":
+        package_manager_command = "brew install "
     else:
-        if opsy_name == "Linux":
-            pacman_names = ["apt-get install -y ", "customizer-install ", "flatpak install ", "snap install "]
-            # This value might vary between flatpak, snapd, apt-get or Linux-Auto-Customizer
-            # For the moment expects a valid app name and may need root (sudo) privileges
-            package_manager_command = pacman_names[1]
-        elif opsy_name == 'Windows ':
-            package_manager_command = "winget install "
-            winget_allowance_command = True
-        elif opsy_name == "Darwin":
-            package_manager_command = "brew install "
-    
+        '''
+        print("hola")
+        if subprocess.run("uname -o").strip() == 'Android':
+            package_manager_command = "pm install "
+            '''
+        pass
     for i in range(len(argument_list)):
-        final_query += " " + argument_list[i]
+        final_query.append(argument_list[i])
+        quer += " " + argument_list[i]
+    # Argument processing (&& in Linux ; in windows)
 
-    quer = package_manager_command + final_query
-    subprocess.run(quer, shell=True)
+    if not winget_allowance_command:
+        quer = package_manager_command + quer
+        subprocess.run(quer, shell=True)
+    else:
+        #quer += ";"
+        # Windows is going to need some logic to install by id performing some research to form the string
+
+        #print(final_query)
+        for i in final_query:
+            quer = package_manager_command + " " + i
+            subprocess.run(quer, shell=True)
 
 
 if __name__ == "__main__":
