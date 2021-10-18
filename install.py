faf950efa7581da955e361e80a312e020aa0f03a
Axel <axfe999@vidalibarraquer.net>
2021-10-18 08:32:27 +0200

    UPDATED: install .py organization

--- install.py
+++ install.py
@@ -12,17 +12,19 @@ def main():
     quer = ""
     query_list = []
     final_query = []
-    
+
     if opsy_name == "Linux":
         pacman_names = ["apt-get install -y ", "customizer-install", "flatpak install ", "zypper --non-interactive install -y ", "dnf install -y ", "urpme ", "slackpkg install ", "slapt-get --install ", "netpkg ", "equo install ", "pacman -S ", "eopkg install ", "apk add ", "smart install ", "pkcon install ", "emerge ", "lin ", "cast ", "nix-env -i ", "xbps-install ", "snap install ", "pkg_add -r ", "pkg install " ]
 
         package_manager_command = pacman_names[1]
     elif opsy_name == "Windows":
-        package_manager_command = "winget install --accept-package-agreements --accept-source-agreements -h -q "
+        pacman_names = ["winget install --accept-package-agreements --accept-source-agreements -h -q ", "choco install -y "]
+        package_manager_command = pacman_names[0]
         winget_allowance_command = True
     elif opsy_name == "Darwin":
         package_manager_command = "brew install "
     else:
+        # This expects a path to an .apk file and root (sudo) privileges assuming rooted android device
         if subprocess.run("uname -o").strip() == 'Android':
             package_manager_command = "pm install "
 
@@ -35,7 +37,6 @@ def main():
         quer = package_manager_command + quer
         subprocess.run(quer, shell=True)
     else:
-        # This expects a path to an .apk file and root (sudo) privileges assuming rooted android device
         for i in final_query:
             quer = package_manager_command + " " + i
             subprocess.run(quer, shell=True)
