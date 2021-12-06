ab7e12245b595bfbbe3eef48c5c2d9129899142d
Axlfc <axelfernandezcurros@gmail.com>
2021-12-07 00:26:37 +0100

    UPDATED: scripts_source_list.sh relative paths

--- scripts/script_source_list.sh
+++ scripts/script_source_list.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 # source the path of this script in user's bashrc or profile to be able to use bash functions
-item_list=("/home/axel/Escritorio/git/ScriptsStudio/scripts/manage_apt_repo.sh" 
-"/home/axel/Escritorio/git/ScriptsStudio/scripts/customize_desktop.sh" 
-"/home/axel/Escritorio/git/ScriptsStudio/scripts/rename.sh" 
-"/home/axel/Escritorio/git/ScriptsStudio/scripts/compile_rust.sh"
-"/home/axel/Escritorio/git/ScriptsStudio/scripts/wtf.sh")
+if [ ! -f "$HOME/.config/user-dirs.dirs" ]; then
+  xdg-user-dirs-update
+else
+  if [ -f "$HOME/.config/user-dirs.dirs" ]; then
+    source "$HOME/.config/user-dirs.dirs"
+  fi
+fi
+item_list=("${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/manage_apt_repo.sh"
+"${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/customize_desktop.sh"
+"${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/rename.sh"
+"${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/compile_rust.sh"
+"${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/wtf.sh"
+)
 
 
-for item in ${item_list[@]}; do
-    if [ -f "${item}" ]; then
-        source "${item}"
-    fi
+for item in "${item_list[@]}"; do
+  source "${item}"
 done
