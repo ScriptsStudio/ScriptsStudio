aef43439c30b409d9670c4522ff90ccb19669b57
Axlfc <axelfernandezcurros@gmail.com>
2021-12-07 15:28:53 +0100

    UPDATED: script_source_list.sh unset relative variables

--- scripts/script_source_list.sh
+++ scripts/script_source_list.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 # source the path of this script in user's bashrc or profile to be able to use bash functions
-if [ ! -f "$HOME/.config/user-dirs.dirs" ]; then
-  xdg-user-dirs-update
-else
-  if [ -f "$HOME/.config/user-dirs.dirs" ]; then
-    source "$HOME/.config/user-dirs.dirs"
+
+
+if [ "${EUID}" != 0 ]; then
+  declare HOME_FOLDER="${HOME}"
+  declare BASHRC_PATH="${HOME_FOLDER}/.bashrc"
+  declare BASHRC_ALL_USERS_PATH="/etc/bash.bashrc"
+  declare PROFILE_PATH="${HOME_FOLDER}/.profile"
+
+  declare USER_DIRS_PATH="${HOME_FOLDER}/.config/user-dirs.dirs"
+
+  if [ ! -f "${USER_DIRS_PATH}" ]; then
+    xdg-user-dirs-update
+  else
+    if [ -f "${USER_DIRS_PATH}" ]; then
+      source "${USER_DIRS_PATH}"
+    fi
   fi
 fi
+
+
 item_list=("${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/manage_apt_repo.sh"
 "${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/customize_desktop.sh"
 "${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/rename.sh"
@@ -18,3 +31,26 @@ item_list=("${XDG_DESKTOP_DIR}/git/ScriptsStudio/scripts/manage_apt_repo.sh"
 for item in "${item_list[@]}"; do
   source "${item}"
 done
+
+
+if [ "${EUID}" != 0 ]; then
+  declare SCRIPTS_FOLDER="${XDG_DESKTOP_DIR}/git/ScriptsStudio"
+  declare SCRIPTS_SCRIPT_PATH="${SCRIPTS_FOLDER}/scripts/script_source_list.sh"
+  chmod +x "${SCRIPTS_SCRIPT_PATH}"
+
+  # This script maintains a source line script in ~/.bashrc
+  declare script_source_list="source ${SCRIPTS_SCRIPT_PATH}"
+  if ! grep -Fqo "${script_source_list}" "${BASHRC_PATH}"; then
+    echo -e "${script_source_list}" >> "${BASHRC_PATH}"
+  fi
+
+  unset script_source_list
+
+  unset HOME_PATH
+  unset BASHRC_PATH
+  unset BASHRC_ALL_USERS_PATH
+  unset PROFILE_PATH
+  unset USER_DIRS_PATH
+  unset SCRIPTS_FOLDER
+  unset SCRIPTS_SCRIPT_PATH
+fi
