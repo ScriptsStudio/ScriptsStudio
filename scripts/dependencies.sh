a6b32beb969af1bc5c365717b1b510d0d033e10d
Axel <axfe999@vidalibarraquer.net>
2022-02-22 12:30:34 +0100

    UPDATED: dependencies.sh creates folder in /home/axel/ScriptsStudio and copy install.py file to it

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 dist="$(cat /etc/*-release | cut -d "=" -f2 | head -1)"
+DIR="$(echo "${BASH_SOURCE}" | rev | cut -d '/' -f3- | rev)"
+
 if [ "${dist}" == "Ubuntu" ]; then
     pacman="apt-get install -y"
+    if [ ! -d "${HOME}/ScriptsStudio" ]; then
+      mkdir "${HOME}/ScriptsStudio"
+    fi
+    if [ ! -f "${HOME}/ScriptsStudio/install.py" ]; then
+      cp "../install.py" "${HOME}/ScriptsStudio/install.py"
+    fi
 else
-    pacman="apt-get install -y"
+    #pacman="apt-get install -y"
+    :
 fi
-${pacman} python3 ssh samba
+#${pacman} python3 ssh samba
